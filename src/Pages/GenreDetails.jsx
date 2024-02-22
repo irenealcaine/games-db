6053ffb0f3ea0917a0269268528e2aab6db44ce2
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-22 12:59:49 +0100

    Add loader component and loading state to GenreDetails and Genres pages

--- src/Pages/GenreDetails.jsx
+++ src/Pages/GenreDetails.jsx
@@ -2,21 +2,28 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Layout from '../Components/Layout'
+import Loader from '../Components/Loader'
 
 const GenreDetails = () => {
 
   const { id } = useParams()
   const [genre, setGenre] = useState([])
+  const [loading, setLoading] = useState(true)
   const key = import.meta.env.VITE_API_KEY
 
   useEffect(() => {
+    setLoading(true)
     axios.get(`https://api.rawg.io/api/genres/${id}?key=${key}`)
       .then((res) => {
         // console.log(res.data)
         setGenre(res.data)
+        setLoading(false)
+
       })
       .catch((error) => {
         console.log(error)
+        setLoading(false)
+
       })
   }, [])
 
@@ -26,6 +33,7 @@ const GenreDetails = () => {
 
   return (
     <Layout>
+      {loading && <Loader />}
       <h1>{genre.name}</h1>
       <img src={genre.image_background} alt={genre.name} />
       <p>Games: {genre.games_count}</p>
--- src/Pages/Genres.jsx
+++ src/Pages/Genres.jsx
@@ -4,26 +4,34 @@ import axios from 'axios';
 import requests from '../requests';
 import { Link } from 'react-router-dom';
 import Layout from '../Components/Layout'
+import Loader from '../Components/Loader';
 
 const Genres = () => {
 
   const [genres, setGenres] = useState([])
+  const [loading, setLoading] = useState(true)
   const { theme } = useTheme();
 
   useEffect(() => {
+    setLoading(true)
     axios.get(requests.allGenres)
       .then((res) => {
         // console.log(res.data.results)
         setGenres(res.data.results)
         // console.log(genres)
+        setLoading(false)
+
       })
       .catch((error) => {
         console.log(error)
+        setLoading(false)
+
       })
   }, [])
 
   return (
     <Layout>
+      {loading && <Loader />}
       {genres.map((genre) => (
         <Link to={`/genre/${genre.id}`} key={genre.id}>
           <h2>{genre.name}</h2>
