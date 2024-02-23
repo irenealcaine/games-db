16f44ee1ff9e68def34b9a25124c82998d3190be
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-23 12:55:51 +0100

    Refactor class to className in Loader component

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -6,11 +6,11 @@ const Loader = () => {
   const { theme } = useTheme();
 
   return (
-    <div class='flex space-x-4 justify-center items-center py-16'>
-      <span class='sr-only'>Loading...</span>
-      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.3s]`}></div>
-      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.15s]`}></div>
-      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping`}></div>
+    <div className='flex space-x-4 justify-center items-center py-16'>
+      <span className='sr-only'>Loading...</span>
+      <div className={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.3s]`}></div>
+      <div className={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.15s]`}></div>
+      <div className={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping`}></div>
     </div>
   )
 }
