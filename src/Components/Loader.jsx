ab052b15323d4091271c965c6bf10bc60c938ab8
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-23 12:34:49 +0100

    Update Loader animation

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -8,9 +8,9 @@ const Loader = () => {
   return (
     <div class='flex space-x-4 justify-center items-center py-16'>
       <span class='sr-only'>Loading...</span>
-      <div class={`h-6 w-6 bg-${theme}-700 rounded-full animate-bounce [animation-delay:-0.3s]`}></div>
-      <div class={`h-6 w-6 bg-${theme}-700 rounded-full animate-bounce [animation-delay:-0.15s]`}></div>
-      <div class={`h-6 w-6 bg-${theme}-700 rounded-full animate-bounce`}></div>
+      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.3s]`}></div>
+      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping [animation-delay:-0.15s]`}></div>
+      <div class={`h-4 w-4 bg-${theme}-700 rounded-full animate-ping`}></div>
     </div>
   )
 }
