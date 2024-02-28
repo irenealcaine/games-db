93a633af3298494033756348e8ca51699c3233c7
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-28 16:00:02 +0100

    Update Hero and Navbar components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,18 +8,28 @@ const Navbar = () => {
   ]
 
   const { toggleTheme, theme } = useTheme();
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className={`h-full w-full md:w-40 fixed backdrop-blur-sm bg-neutral-950/90 md:bg-neutral-950 flex flex-col justify-between items-center p-4 z-10`}>
+    <div className={`md:h-full md:w-40 fixed backdrop-blur-sm bg-neutral-950/90 md:bg-neutral-950 flex flex-col justify-between items-center p-4 z-10 transition
+    ${open ? 'h-full w-full' : ''}
+    `}>
+
       <Link to={`/`} className={`hidden md:inline text-${theme}-700 text-xl font-extrabold`}>GDB</Link>
-      <Link to={`/`} className={`md:hidden inline text-${theme}-700 text-xl font-extrabold`}>X</Link>
-      <div className={`flex flex-col px-4 *:transition-all`}>
+
+      <p
+        onClick={() => setOpen(!open)}
+        className={`md:hidden inline text-${theme}-700 text-xl font-extrabold`}>
+        {open ? 'X' : 'O'}
+      </p>
+
+      <div className={`flex flex-col px-4 *:transition-all ${open ? '' : 'hidden'}`}>
         <Link to={`/`} className={`text-${theme}-50 hover:text-${theme}-400`}>Home</Link>
         <Link to={`/genres`} className={`text-${theme}-50 hover:text-${theme}-400`}>Genres</Link>
         <Link to={`/developers`} className={`text-${theme}-50 hover:text-${theme}-400`}>Developers</Link>
         <Link to={`/tags`} className={`text-${theme}-50 hover:text-${theme}-400`}>Tags</Link>
       </div>
-      <div className='flex flex-col '>
+      <div className={`flex flex-col ${open ? '' : 'hidden'}`}>
         {colors.map((color) => (
           <div key={color} className={`w-4 h-4 rounded-full bg-${color}-900 border border-${color}-500`} onClick={() => toggleTheme(color)}></div>
         ))}
