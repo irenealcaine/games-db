0bf63e48e4014e2ec1d4e8d8ae6d13abb17c2b2c
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-29 20:33:10 +0100

    Remove ViteFaviconsPlugin from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { ViteFaviconsPlugin } from "module";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteFaviconsPlugin('/game-controller.svg')
   ],
 })
