9a619af79915e57fd0e634d9ad97970b04943830
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-29 17:10:26 +0100

    Fix platform search bug in BestGames component

--- src/Pages/BestGames.jsx
+++ src/Pages/BestGames.jsx
@@ -21,7 +21,7 @@ const BestGames = () => {
 
     Promise.all([
       axios.get(requests.allPlatforms),
-      axios.get(requests.bestGames + `&search=${search}&${platforms && `platforms=${platformSearch}`}`)
+      axios.get(requests.bestGames + `&search=${search}${platformSearch && `&platforms=${platformSearch}`}`)
     ])
       .then(([platformsResponse, gamesResponse]) => {
         setPlatforms(platformsResponse.data.results)
@@ -40,6 +40,7 @@ const BestGames = () => {
 
       <input type="text" onChange={(e) => (setSearch(e.target.value))} />
       <select name="" id="" onChange={(e) => (setPlatformSearch(e.target.value))}>
+        <option value="" >All Platforms</option>
         {platforms && platforms.map((platform) => (
           <option key={platform.id} value={platform.id}>{platform.name}</option>
         ))}
@@ -49,11 +50,11 @@ const BestGames = () => {
       {loading && <Loader />}
       <div className='columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-x-0 pt-8 '>
         {games.map((game) => (
-          <div className={` rounded-xl overflow-hidden px-4 pb-4 group`}>
+          <div key={game.id} className={` rounded-xl overflow-hidden px-4 pb-8 group`}>
             <Link to={`/game/${game.id}`} key={game.id}>
               <h2 className='m-2 text-xl font-bold group-hover:underline'>{game.name}</h2>
               <img className={`w-full aspect-video object-cover rounded-xl border border-${theme}-500 shadow-neon shadow-${theme}-700 group-hover:scale-105 transition-all`} src={game.background_image || placeholder} alt={game.name} />
-              <div className='absolute group-hover:-translate-y-6 opacity-0 group-hover:opacity-100 transition-all bg-neutral-950 py-1 px-4 rounded-xl'>
+              <div className='absolute group-hover:-translate-y-6 opacity-0 max-w-72 group-hover:opacity-100 transition-all bg-neutral-950 py-1 px-4 rounded-xl'>
                 <div className='flex flex-wrap gap-2 items-center'>
                   {game.metacritic && <RadialProgress radius={12} percentage={game.metacritic} />}
                   {game.parent_platforms.map((platform) => (
