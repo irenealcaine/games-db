0bceb3451db40145c6a90acccbd9489274208337
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-03-06 13:02:39 +0100

    Update LinkButton styling

--- src/Components/LinkButton.jsx
+++ src/Components/LinkButton.jsx
@@ -7,7 +7,7 @@ const LinkButton = ({ to, value }) => {
   const { theme } = useTheme()
 
   return (
-    <Link to={to} className={`bg-${theme}-900 hover:bg-${theme}-700 border border-${theme}-500 px-4 py-1 rounded-lg transition-all`}>{value}</Link>
+    <Link to={to} className={`bg-${theme}-900 bg-opacity-50 hover:bg-opacity-100 hover:bg-${theme}-700 border-2 border-${theme}-500 px-4 py-1 rounded-lg transition-all`}>{value}</Link>
   )
 }
 
