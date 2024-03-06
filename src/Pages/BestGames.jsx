548a340f63c5ac2ec906b8499961b80b11e9dcee
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-03-06 16:36:44 +0100

    Refactor game card layout and styling

--- src/Pages/BestGames.jsx
+++ src/Pages/BestGames.jsx
@@ -49,9 +49,6 @@ const BestGames = () => {
         </Select>
       </div>
 
-
-
-
       {loading && <Loader />}
       <div className='columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-4 pt-8 '>
         {games.map((game) => (
@@ -59,7 +56,7 @@ const BestGames = () => {
             <Link to={`/game/${game.id}`} key={game.id}>
               <h2 className='m-2 text-xl font-bold group-hover:underline'>{game.name}</h2>
               <img className={`w-full aspect-video object-cover rounded-xl border border-${theme}-500 shadow-neon shadow-${theme}-700 group-hover:scale-105 transition-all`} src={game.background_image || placeholder} alt={game.name} />
-              <div className='absolute group-hover:-translate-y-6 opacity-0 max-w-72 group-hover:opacity-100 transition-all bg-neutral-950 py-1 px-4 rounded-xl'>
+              <div className='absolute group-hover:-translate-y-8 opacity-0 max-w-72 group-hover:opacity-100 transition-all bg-neutral-950 py-1 px-4 rounded-xl'>
                 <div className='flex flex-wrap gap-2 items-center'>
                   {game.metacritic && <RadialProgress radius={12} percentage={game.metacritic} />}
                   {game.parent_platforms.map((platform) => (
@@ -68,7 +65,7 @@ const BestGames = () => {
                 </div>
               </div>
 
-              <div className='flex justify-end gap-2 m-2 my-4'>
+              <div className='flex justify-end flex-wrap gap-2 m-2 my-4'>
                 {game.genres.map((genre) => (
                   <span key={genre.id} className={`px-2 border border-${theme}-500 rounded-full`}>{genre.name}</span>
                 ))}
