41f195090d817fddef29a071a909e9e6184caebf
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-03-06 17:08:30 +0100

    Refactor Genres component and update API request for allGenres

--- src/Pages/Genres.jsx
+++ src/Pages/Genres.jsx
@@ -3,7 +3,6 @@ import { useTheme } from '../Context/themeContext';
 import axios from 'axios';
 import requests from '../requests';
 import { Link } from 'react-router-dom';
-import Layout from '../Components/Layout'
 import Loader from '../Components/Loader';
 
 const Genres = () => {
@@ -31,18 +30,22 @@ const Genres = () => {
   return (
     <main>
       {loading && <Loader />}
-      {genres.map((genre) => (
-        <div key={genre.id}>
-          <Link to={`/genre/${genre.id}`} >
-            <h2>{genre.name}</h2>
-            <img src={genre.image_background} alt={genre.name} className='' />
-          </Link>
-          <p>Related games: {genre.games_count}</p>
-          {genre.games.map((game) => (
-            <Link to={`/game/${game.id}`} key={game.id}>{game.name}</Link>
-          ))}
-        </div>
-      ))}
+      <div className='flex flex-wrap gap-4'>
+        {genres.map((genre) => (
+          <div key={genre.id} className='relative border w-72 aspect-video rounded-lg overflow-hidden group'>
+            <Link to={`/genre/${genre.id}`} >
+              <h2 className={`absolute top-4 left-4 z-20 py-1 px-4 bg-${theme}-900 bg-opacity-80 rounded-md group-hover:underline`}>{genre.name}</h2>
+              <img src={genre.image_background} alt={genre.name} className='absolute top-0 w-full aspect-video object-cover z-10 group-hover:scale-105 transition-all' />
+            </Link>
+            {/* <p>Related games: {genre.games_count}</p> */}
+            {/* <div className='flex gap-2 flex-wrap'>
+              {genre.games.map((game) => (
+                <Link to={`/game/${game.id}`} key={game.id}>{game.name}</Link>
+              ))}
+            </div> */}
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
--- src/requests.js
+++ src/requests.js
@@ -5,7 +5,7 @@ const requests = {
   lastGames: `https://api.rawg.io/api/games?key=${key}&page_size=20&ordering=-released`,
   nextGames: `https://api.rawg.io/api/games?key=${key}&page_size=20&ordering=released`,
   bestGames: `https://api.rawg.io/api/games?key=${key}&page_size=20&ordering=-metacritic`,
-  allGenres: `https://api.rawg.io/api/genres?key=${key}`,
+  allGenres: `https://api.rawg.io/api/genres?key=${key}&page_size=40`,
   allDevelopers: `https://api.rawg.io/api/developers?key=${key}`,
   allTags: `https://api.rawg.io/api/tags?key=${key}`,
   allPlatforms: `https://api.rawg.io/api/platforms?key=${key}`,
